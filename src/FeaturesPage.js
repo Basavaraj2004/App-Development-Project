30b59d4ad2b47a58b0fabb7cf0b1dfca3731a5f9
Basavaraj <sanjaybasa3@gmail.com>
2024-07-24 20:23:29 +0530

    update

---
+++ src/FeaturesPage.js
@@ -0,0 +1,13 @@
+// src/FeaturesPage.js
+import React from 'react';
+
+const FeaturesPage = () => {
+  return (
+    <div className="features-wrapper">
+      <h1>Features</h1>
+      <p>Information on features.</p>
+    </div>
+  );
+};
+
+export default FeaturesPage;
